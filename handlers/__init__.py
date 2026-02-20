65ee7569f5bfba018cad2443697dbbf5bf596606
egoza17499 <meha99.74@yandex.ru>
2026-02-21 01:38:31 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -7,3 +7,13 @@ from . import search
 from . import welcome
 
 __all__ = ['registration', 'menu', 'profile', 'admin', 'search', 'welcome']
+
+# Функция для инициализации роутеров с БД
+def init_routers(db_instance):
+    """Передаём экземпляр БД во все роутеры"""
+    registration.db = db_instance
+    menu.db = db_instance
+    profile.db = db_instance
+    admin.db = db_instance
+    search.db = db_instance
+    welcome.db = db_instance
