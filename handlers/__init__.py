f8d5f618774062477d109dd4a79129c974bb9809
egoza17499 <meha99.74@yandex.ru>
2026-02-21 00:04:21 +0300

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,9 @@
+# Пакет обработчиков
+from . import registration
+from . import menu
+from . import profile
+from . import admin
+from . import search
+from . import welcome
+
+__all__ = ['registration', 'menu', 'profile', 'admin', 'search', 'welcome']
